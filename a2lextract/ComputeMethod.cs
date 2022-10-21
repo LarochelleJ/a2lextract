2ab69a3a827de28433ce538864fd5df393eb814f
LarochelleJ <larochelle.jason@live.fr>
2022-10-21 09:23:01 -0400

    Ajoutez des fichiers projet.

---
+++ a2lextract/ComputeMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a2lextract {
+    internal class ComputeMethod {
+        public string unit { get; }
+        public string offset { get; }
+        public string factor { get; }
+        public ComputeMethod(string unit, string[] coeffs) {
+            this.unit = unit;
+            offset = coeffs[3];
+            factor = coeffs[6];
+        }
+    }
+}
